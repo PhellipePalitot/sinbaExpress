326a62c18b90ab490454aa776747dea3824a50be
Dhanilow15 <dhanilow14@gmail.com>
2023-10-03 19:05:28 -0300

    chore (sinba.py) menu skeleton created 03.10.2023

--- Sinba/sinba.py
+++ Sinba/sinba.py
@@ -1,6 +1,33 @@
 from connection import Connection
 import getpass
 
+
+def display_menu(connect: Connection):
+    while True:
+        print("\nSinbaExpress Menu:")
+        print("1. Create")
+        print("2. Read")
+        print("3. Update")
+        print("4. Delete")
+        print("5. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            connect.create()
+        elif choice == '2':
+            connect.read()
+        elif choice == '3':
+            connect.update()
+        elif choice == '4':
+            connect.delete()
+        elif choice == '5':
+            print("Exiting the CRUD menu. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please select a valid option.")
+
+
 class SinbaExpress():
     def __init__(self):
         return
@@ -8,5 +35,6 @@ class SinbaExpress():
     def iniciar(self):
         db_password = getpass.getpass("Enter the password to access the DB: ")
         connection = Connection(db_password)
+        display_menu(connect=connection)
         print(connection.read_all("Clientes"))
         return
